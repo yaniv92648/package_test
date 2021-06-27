12615cb31cf5f7b8d26c47e5ea168f7d17a58cfe
yaniv92648 <yaniv92648@gmail.com>
2021-06-27 14:34:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gitandpip",
+    name="code",
     version="0.0.1",
-    author="arocketman",
-    author_email="and.capuano@gmail.com",
+    author="Yaniv Cohen",
+    author_email="yaniv92648@gmail.com",
     description="It's pip... with git.",
     long_description=long_description,
-    url="https://github.com/arocketman/git-and-pip",
+    url="https://github.com/yaniv92648/package_test",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
